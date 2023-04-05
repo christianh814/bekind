a3cd14ba785c0949392b5e0d96bcd3a96e4b5cef
Christian Hernandez <christian@chernand.io>
2023-04-05 14:59:10 -0700

    updated to cobra 1.7 and viper 1.15. updated to kind v0.18. Added showConfig subcommand
    Signed-off-by: Christian Hernandez <christian@chernand.io>

--- cmd/destory.go
+++ cmd/destory.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,8 +23,9 @@ import (
 
 // destroyCmd represents the destory command
 var destroyCmd = &cobra.Command{
-	Use:   "destroy",
-	Short: "Destroys the custom Kind cluster",
+	Use:     "destroy",
+	Aliases: []string{"delete", "del"},
+	Short:   "Destroys the custom Kind cluster",
 	Long: `Destroys a running custom Kind cluster. Currently
 it only destroys the named cluster or it will destroy ones names "kind"
 if one isn't named.`,
---
+++ cmd/showconfig.go
@@ -0,0 +1,49 @@
+/*
+Copyright © 2023 Christian Hernandez <christian@chernand.io>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"gopkg.in/yaml.v3"
+)
+
+// showconfigCmd represents the showconfig command
+var showconfigCmd = &cobra.Command{
+	Use:     "showconfig",
+	Aliases: []string{"sc", "showConfig", "configShow"},
+	Short:   "Prints out the config that will be used",
+	Long: `Prints out the config that will be used by
+bekind to set up your local Kind cluster.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Marshal in the entire config file int a byteslice and check for errors
+		byteSlice, err := yaml.Marshal(viper.AllSettings())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Print it out
+		fmt.Print(string(byteSlice))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(showconfigCmd)
+}
