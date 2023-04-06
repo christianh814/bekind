818bcafc7a30d5e84069845cc769eebbdd8eeacc
usrbinkat <kathryn.morgan@braincraft.io>
2023-04-05 19:16:07 -0700

    wip refactor
    Signed-off-by: usrbinkat <kathryn.morgan@braincraft.io>

--- cmd/destroy.go
+++ cmd/destroy.go
@@ -21,7 +21,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
-	"github.com/usrbinkat/bekind/pkg/clusterops"
+	"github.com/usrbinkat/bekind/pkg/cluster"
 )
 
 // destroyCmd represents the destroy command
@@ -48,7 +48,7 @@ func init() {
 func destroyCluster() error {
 	clusterName := viper.GetString("clusterName")
 
-	err := clusterops.DeleteKindCluster(clusterName, "")
+	err := cluster.DeleteKindCluster(clusterName, "")
 	if err != nil {
 		return fmt.Errorf("failed to destroy KIND cluster: %w", err)
 	}
