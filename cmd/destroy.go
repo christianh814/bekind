d8b6946db6e7ee703143482b9ecdccfa00644b60
usrbinkat <kathryn.morgan@braincraft.io>
2023-04-05 17:16:57 -0700

    fix naming
    Signed-off-by: usrbinkat <kathryn.morgan@braincraft.io>

--- cmd/destroy.go
+++ cmd/destroy.go
@@ -21,7 +21,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
-	"github.com/usrbinkat/bekinder/pkg/clusterops"
+	"github.com/usrbinkat/bekind/pkg/clusterops"
 )
 
 // destroyCmd represents the destroy command
--- cmd/showconfig.go
+++ cmd/showconfig.go
@@ -21,7 +21,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
-	"github.com/usrbinkat/bekinder/pkg/config"
+	"github.com/usrbinkat/bekind/pkg/config"
 )
 
 // showConfigCmd represents the showconfig command
